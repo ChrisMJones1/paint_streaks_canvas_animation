b8794362fe9f5859bcd216fd53454cc43a326001
Christopher Jones <chrisjns679@gmail.com>
2020-06-14 14:59:09 -0400

    Added window size readjustment handling, and extra color palettes to the pool of potential colors.

--- scripts/script.js
+++ scripts/script.js
@@ -65,19 +65,13 @@ let r;
 
 function animationStart() {
     window.onresize = resize;
-    for(let i = (2*radius + gap); i < clientWidth - (2*radius + gap); i += (2*radius + gap)) {
-        r = 0;
-        for(let j = (2*radius + gap); j <= clientHeight - (2*radius + gap); j += (2*radius + gap)) {
-            r++;
-            if(Math.random() > 0.6) {
-                let circ = new Circle(i,j,r);
-                circles.push(circ);
-            }
-
-
-        }//vert inner loop
-
-    }//horz outer loop
+    for(let i = 0; i < clientWidth + 2 * (2*radius); i += (2*radius)) {
+        if(Math.random() > 0.6) {
+            let radiusAdjustment = Math.random() > 0.5 ? (Math.random() * radius) / 2 * -1 : (Math.random() * radius) / 2;
+            let circ = new Circle(i, 0, r, radius + radiusAdjustment);
+            circles.push(circ);
+        }
+    }
     animationFrame = window.requestAnimationFrame(animationLoop);
 }
 
@@ -88,8 +82,16 @@ const colorPalletes = [
     ["#03071e","#370617","#6a040f","#9d0208","#d00000","#dc2f02","#e85d04","#f48c06","#faa307","#ffba08"],
     ["#7400b8","#6930c3","#5e60ce","#5390d9","#4ea8de","#48bfe3","#56cfe1","#64dfdf","#72efdd","#80ffdb"],
     ["#9b5de5","#f15bb5","#fee440","#00bbf9","#00f5d4"],
+    ["#2d00f7","#6a00f4","#8900f2","#a100f2","#b100e8","#bc00dd","#d100d1","#db00b6","#e500a4","#f20089"],
+    ["#ef476f","#ffd166","#06d6a0","#118ab2","#073b4c"],
+    ["#ffbe0b","#fb5607","#ff006e","#8338ec","#3a86ff"],
+    ["#0c0f0a","#ff206e","#fbff12","#41ead4","#ffffff"],
+    ["#b76935","#a56336","#935e38","#815839","#6f523b","#5c4d3c","#4a473e","#38413f","#263c41","#143642"],
+    ["#ffffff","#81f4e1","#56cbf9","#ff729f","#d3c4d1"],
+    ["#4d9de0","#e15554","#e1bc29","#3bb273","#7768ae"],
 
-    
+
+
 
 ];
 
@@ -179,7 +181,9 @@ function resize() {
 
     ctx.canvas.width  = pageWidth;
     ctx.canvas.height = pageHeight;
-
+    clientHeight = pageHeight;
+    clientWidth = pageWidth;
+    
 }
 
 
